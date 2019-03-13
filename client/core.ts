0ed0f2ec7f110bdb96437696cfd67de6147d6223
dousha <lijiahao34@gmail.com>
2019-03-13 21:31:27 +0800

    having some fun

---
+++ client/core.ts
@@ -0,0 +1,10 @@
+import { WindowManager, UserWindow, Label } from "./ui";
+
+let canvas = document.getElementById("main") as HTMLCanvasElement;
+let wm = new WindowManager(canvas);
+let win = new UserWindow("Test", 400, 300);
+let lbl1 = new Label(0, 0, "test", {});
+win.addComponent(lbl1);
+wm.addWindow(win);
+wm.resizeCanvas();
+wm.update();
--- client/ui.ts
+++ client/ui.ts
@@ -1,4 +1,4 @@
-class WindowManager {
+export class WindowManager {
 	public constructor(canvas: HTMLCanvasElement) {
 		this.canvas = canvas;
 		this.resizeCanvas();
@@ -11,7 +11,10 @@ class WindowManager {
 		let ctx = this.canvas.getContext("2d");
 		if (ctx) {
 			for (let w of this.windows) {
+				ctx.save();
+				ctx.translate(w.X, w.Y);
 				w.render(ctx);
+				ctx.restore();
 			}
 		} else {
 			alert("WTF?");
@@ -35,14 +38,26 @@ class WindowManager {
 	}
 
 	public removeWindow(index: number) {
-		this.windows.splice(index, 1);
+		let windows = this.windows.splice(index, 1);
+		for (let window of windows) {
+			window.onClose();
+		}
+	}
+
+	public get isDebugging() {
+		return this.debugging;
+	}
+
+	public set isDebugging(b: boolean) {
+		this.debugging = b;
 	}
 
 	private canvas: HTMLCanvasElement;
 	private windows: Array<UserWindow> = new Array<UserWindow>();
+	private debugging = false;
 }
 
-class UserWindow {
+export class UserWindow {
 	public constructor(title: string, height: number, width: number) {
 		this.title = title;
 		this.height = height;
@@ -50,24 +65,67 @@ class UserWindow {
 	}
 
 	public render(ctx: CanvasRenderingContext2D) {
+		// draw a box
+		ctx.fillStyle = "black";
+		ctx.strokeStyle = "black";
+		ctx.font = '12px monospace';
+		ctx.strokeRect(0, 0, this.width, 20);
+		ctx.fillText(this.title, 3, 3);
+		ctx.strokeRect(0, 20, this.width, this.height);
+		// translate into the stuff
+		ctx.save();
+		ctx.translate(this.x, this.y);
+		// then draw components
 		for (let component of this.components) {
 			if (component.isVisible) {
 				component.render(ctx);
 			}
 		}
+		ctx.restore();
 	}
 
 	public addComponent(component: Component) {
 		this.components.push(component);
 	}
 
+	public moveTo(x: number, y: number) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public resizeTo(w: number, h: number) {
+		this.width = w;
+		this.height = h;
+	}
+
+	public setTitle(title: string) {
+		this.title = title;
+	}
+
+	public onClose() { }
+
+	public get requestForFocus() {
+		return this.needFocus;
+	}
+
+	public get X() {
+		return this.x;
+	}
+
+	public get Y() {
+		return this.y;
+	}
+
+	private x = 0;
+	private y = 0;
 	private title: string;
 	private height: number;
 	private width: number;
 	private components: Array<Component> = new Array<Component>();
+	private needFocus = false;
 }
 
-abstract class Component {
+export abstract class Component {
 	protected constructor(x: number, y: number, width: number, height: number, visible: boolean = true) {
 		this.x = x;
 		this.y = y;
@@ -125,24 +183,40 @@ abstract class Component {
 	protected visible: boolean;
 }
 
-class Label extends Component {
-	public constructor(x: number, y: number, caption: string, options: {
-		font: "monospace",
-		size: 16,
-		color: "#000",
-		background: "#FFF",
-		width: 200
+export class Label extends Component {
+	public constructor(x: number, y: number, caption: string, {
+		font = "monospace",
+		size = 16,
+		color = "#000",
+		background = "#FFF",
+		width = 200
 	}) {
-		super(x, y, options.width, options.size);
+		super(x, y, width, size);
 		this.caption = caption;
-		this.options = options;
+		this.font = font;
+		this.size = size;
+		this.color = color;
+		this.background = background;
+		this.width = width;
 	}
 
 	render(ctx: CanvasRenderingContext2D): void {
-		ctx.font = `${this.options.size}px ${this.options.font}`;
+		ctx.font = `${this.size}px ${this.font}`;
+		ctx.fillStyle = this.color;
 		ctx.fillText(this.caption, this.x, this.y, this.width);
 	}
 
+	public get text() {
+		return this.caption;
+	}
+
+	public set text(caption: string) {
+		this.caption = caption;
+	}
+
 	private caption: string;
-	private options: any;
+	private font: string;
+	private size: number;
+	private color: string;
+	private background: string;
 }
