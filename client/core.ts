459342ab2dd1bba398f5d0f60875ed6daed093a9
dousha <lijiahao34@gmail.com>
2019-03-18 20:53:31 +0800

    now with dragging windows

--- client/core.ts
+++ client/core.ts
@@ -2,10 +2,17 @@ import { WindowManager, UserWindow, Label } from "./ui.js";
 
 let canvas = document.getElementById("main") as HTMLCanvasElement;
 let wm = new WindowManager(canvas);
+
 let win = new UserWindow("Test", 400, 300);
 let lbl1 = new Label(0, 0, "test", {});
 win.addComponent(lbl1);
 wm.addWindow(win);
+
+let win2 = new UserWindow("Test2", 300, 200);
+let lbl2 = new Label(0, 0, "666", {});
+win2.addComponent(lbl2);
+wm.addWindow(win2);
+
 wm.resizeCanvas();
 wm.setDebugging(true);
 wm.update();
--- client/ui.ts
+++ client/ui.ts
@@ -10,18 +10,26 @@ export class WindowManager {
 			instance.handleMouseMove(e);
 		};
 		canvas.onmousedown = function (e) {
-			instance.handleMouseClick(e);
-		}
+			instance.handleMouseDown(e);
+		};
+		canvas.onmouseup = function (e) {
+			instance.handleMouseUp(e);
+		};
 	}
 
 	public update() {
 		let ctx = this.canvas.getContext("2d");
 		if (ctx) {
 			for (let w of this.windows) {
+				if (this.fullRepaint) {
+					ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+					this.fullRepaint = false;
+				}
 				ctx.save();
 				ctx.translate(w.getX(), w.getY());
-				ctx.clearRect(0, 0, w.getW(), w.getH());
+				ctx.clearRect(0, 0, w.getW(), w.getH() + w.getTitleBarHeight());
 				w.render(ctx);
+				this.debugging && w.drawDebugBorder(ctx);
 				ctx.restore();
 			}
 			if (this.debugging) {
@@ -46,6 +54,7 @@ export class WindowManager {
 
 	public addWindow(window: UserWindow) {
 		this.windows.push(window);
+		this.fullRepaint = true;
 		return this.windows.length - 1;
 	}
 
@@ -54,6 +63,7 @@ export class WindowManager {
 		for (let window of windows) {
 			window.onClose();
 		}
+		this.fullRepaint = true;
 	}
 
 	public isDebugging() {
@@ -67,17 +77,43 @@ export class WindowManager {
 	public handleMouseMove(e: MouseEvent) {
 		this.mouseX = e.clientX;
 		this.mouseY = e.clientY;
+		if (this.dragWindow && this.mouseButton === 0) {
+			let deltaX = this.mouseX - this.dragMouseX;
+			let deltaY = this.mouseY - this.dragMouseY;
+			this.dragWindow.moveTo(this.dragWindowX + deltaX, this.dragWindowY + deltaY);
+			this.fullRepaint = true;
+		}
 		this.update();
 	}
 
-	public handleMouseClick(e: MouseEvent) {
+	public handleMouseDown(e: MouseEvent) {
+		this.mouseX = e.clientX;
+		this.mouseY = e.clientY;
+		let win = this.getPointingWindow();
+		if (win) {
+			let controls = win.getControlsUnderCursor(this.mouseX - win.getX(), this.mouseY - win.getY());
+			if (controls && controls.length > 0) {
+				controls.forEach(it => it.onClick())
+			} else {
+				this.dragMouseX = this.mouseX;
+				this.dragMouseY = this.mouseY;
+				this.dragWindowX = win.getX();
+				this.dragWindowY = win.getY();
+				this.dragWindow = win;
+				this.setFocus(win);
+			}
+		}
+		this.mouseButton = e.button;
+		this.update();
+	}
 
+	public handleMouseUp(e: MouseEvent) {
+		this.mouseButton = -1;
+		this.dragWindow = undefined;
 	}
 
 	public getPointingWindow(): UserWindow | undefined {
-		let wins = this.windows.filter(it => {
-			it.isCursorInWindow(this.mouseX, this.mouseY);
-		});
+		let wins = this.windows.filter(it => it.isCursorInWindow(this.mouseX, this.mouseY));
 		if (wins && wins.length > 0) {
 			return wins[wins.length - 1]; // the focused window is always at tail
 		} else {
@@ -88,7 +124,7 @@ export class WindowManager {
 	public getPointingControl() {
 		let win = this.getPointingWindow();
 		if (win) {
-			return win.getControlInWindow(this.mouseX - win.getX(), this.mouseY - win.getY());
+			return win.getControlUnderCursor(this.mouseX - win.getX(), this.mouseY - win.getY());
 		}
 	}
 
@@ -99,15 +135,36 @@ export class WindowManager {
 		ctx.textBaseline = "bottom";
 		let corner = ctx.canvas.height - 2;
 		ctx.clearRect(0, corner - 12, ctx.canvas.width, 12);
-		ctx.fillText(`MX: ${this.mouseX}; MY: ${this.mouseY}; WIN: ${this.getPointingWindow()}`, 0, corner);
+		let pointingWindow = this.getPointingWindow();
+		let pointingControl = this.getPointingControl();
+		ctx.fillText(`MX: ${this.mouseX}; MY: ${this.mouseY}; WIN: ${pointingWindow}; CTRL: ${pointingControl} WINFOCUS: ${this.hasFocus(pointingWindow)}`, 0, corner);
+	}
+
+	private hasFocus(win: UserWindow | undefined): boolean {
+		return win != undefined && this.windows[this.windows.length - 1] === win;
+	}
+
+	private setFocus(win: UserWindow) {
+		let i = this.windows.indexOf(win);
+		if (i >= 0) {
+			this.windows.splice(i, 1);
+			this.windows.push(win);
+		}
 	}
 
 	private canvas: HTMLCanvasElement;
 	private windows: Array<UserWindow> = new Array<UserWindow>();
 	private debugging = false;
+	private fullRepaint = false;
 
 	private mouseX = 0;
 	private mouseY = 0;
+	private mouseButton = -1;
+	private dragMouseX = 0;
+	private dragMouseY = 0;
+	private dragWindow?: UserWindow;
+	private dragWindowX = 0;
+	private dragWindowY = 0;
 }
 
 export class UserWindow {
@@ -119,23 +176,30 @@ export class UserWindow {
 
 	public render(ctx: CanvasRenderingContext2D) {
 		// draw a box
-
 		ctx.fillStyle = "black";
 		ctx.strokeStyle = "black";
 		ctx.font = '12px monospace';
 		ctx.textBaseline = "top";
+		ctx.lineWidth = 1;
 		ctx.strokeRect(0, 0, this.width, this.titleBarHeight);
 		ctx.fillText(this.title, 3, 3);
 		ctx.strokeRect(0, this.titleBarHeight, this.width, this.height);
 		// translate into the stuff
 		ctx.save();
-		ctx.translate(this.x, this.y + this.titleBarHeight);
+		ctx.translate(0, this.titleBarHeight);
 		// then draw components
 		for (let component of this.components) {
 			if (component.isVisible) {
 				component.render(ctx);
 			}
 		}
+		ctx.restore();
+	}
+
+	public drawDebugBorder(ctx: CanvasRenderingContext2D) {
+		ctx.save();
+		ctx.translate(0, this.titleBarHeight);
+		this.components.forEach(it => it.drawDebugBorder(ctx));
 		ctx.restore();
 	}
 
@@ -175,14 +239,30 @@ export class UserWindow {
 		return this.height;
 	}
 
+	public getTitleBarHeight() {
+		return this.titleBarHeight;
+	}
+
 	public isCursorInWindow(x: number, y: number) {
 		let xDelta = x - this.x;
 		let yDelta = y - this.y;
 		return xDelta > 0 && yDelta > 0 && xDelta < this.width && yDelta < this.height + this.titleBarHeight;
 	}
 
-	public getControlInWindow(relativeX: number, relativeY: number) {
+	public getControlUnderCursor(relativeX: number, relativeY: number) {
+		let filtered = this.getControlsUnderCursor(relativeX, relativeY);
+		if (filtered && filtered.length > 0) {
+			return filtered[0];
+		}
+		return undefined;
+	}
 
+	public getControlsUnderCursor(relativeX: number, relativeY: number) {
+		return this.components.filter(it => it.isCursorOnControl(relativeX, relativeY - this.titleBarHeight));
+	}
+
+	public needAttention() {
+		return this.needFocus;
 	}
 
 	private x = 0;
@@ -205,6 +285,18 @@ export abstract class Component {
 	}
 
 	public abstract render(ctx: CanvasRenderingContext2D): void;
+
+	public abstract onClick(): void;
+
+	public abstract onHover(): void;
+
+	public abstract onLeave(): void;
+
+	public drawDebugBorder(ctx: CanvasRenderingContext2D) {
+		ctx.strokeStyle = "blue";
+		ctx.lineWidth = 1;
+		ctx.strokeRect(this.x, this.y, this.width, this.height);
+	}
 
 	public getX() {
 		return this.x;
@@ -246,6 +338,13 @@ export abstract class Component {
 		this.visible = visible;
 	}
 
+	public isCursorOnControl(x: number, y: number) {
+		if (!this.visible || this.width < 1 || this.height < 1) return false;
+		let xDelta = x - this.x;
+		let yDelta = y - this.y;
+		return xDelta > 0 && yDelta > 0 && xDelta < this.width && yDelta < this.height;
+	}
+
 	protected x: number;
 	protected y: number;
 	protected width: number;
@@ -283,6 +382,12 @@ export class Label extends Component {
 	public setText(caption: string) {
 		this.caption = caption;
 	}
+
+	public onClick(): void { }
+
+	public onHover(): void { }
+
+	public onLeave(): void { }
 
 	private caption: string;
 	private font: string;
